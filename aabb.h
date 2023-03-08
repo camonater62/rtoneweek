95a1f55d1c338bd1a26e01d3f8ab64cb65b358f9
Cameron West <camonater62@gmail.com>
2023-03-08 13:50:34 -0800

    aabb bounding box

---
+++ aabb.h
@@ -0,0 +1,35 @@
+#ifndef AABB_H
+#define AABB_H
+
+#include "rtweekend.h"
+
+class aabb {
+    public:
+        aabb() {}
+        aabb(const point3& a, const point3& b) { minimum = a; maximum = b; }
+
+        point3 min() const { return minimum; }
+        point3 max() const { return maximum; }
+
+        bool hit(const ray& r, double t_min, double t_max) const;
+
+    public:
+        point3 minimum;
+        point3 maximum;
+};
+
+inline bool aabb::hit(const ray& r, double t_min, double t_max) const {
+    for (int a = 0; a < 3; a++) {
+        auto invD = 1.0 / r.direction()[a];
+        auto t0 = (min()[a] - r.origin()[a]) * invD;
+        auto t1 = (max()[a] - r.origin()[a]) * invD;
+        if (invD < 0)
+            std::swap(t0, t1);
+        t_min = t0 > t_min ? t0 : t_min;
+        t_max = t1 < t_max ? t1 : t_max;
+        if (t_max <= t_min)
+            return false;
+    }
+}
+
+#endif
