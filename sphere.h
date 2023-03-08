d399af28c855d44dfeae85efbec145368c7b2d88
Cameron West <camonater62@gmail.com>
2023-03-08 14:45:28 -0800

    sphere bounding box

--- sphere.h
+++ sphere.h
@@ -12,7 +12,9 @@ class sphere : public hittable {
 
         virtual bool hit(
             const ray& r, double t_min, double t_max, hit_record& rec) const override;
-    
+
+        virtual bool bounding_box(double time0, double time1, aabb& output_box) const override;
+
     public:
         point3 center;
         double radius;
@@ -44,6 +46,12 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
     rec.mat_ptr = mat_ptr;
 
     return true;
+}
+
+bool sphere::bounding_box(double time0, double time1, aabb& output_box) const {
+    output_box = aabb(
+        center - vec3(radius, radius, radius),
+        center + vec3(radius, radius, radius));
 }
 
 #endif
