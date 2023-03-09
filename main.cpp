ec652f610283e75c1ae1138710b6cf278206d0de
Cameron West <camonater62@gmail.com>
2023-03-08 17:15:57 -0800

    marble texture

--- main.cpp
+++ main.cpp
@@ -103,7 +103,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 20;
+    const int samples_per_pixel = 100;
     const int max_depth = 50;
 
     // World
