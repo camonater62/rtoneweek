049ba1fc352058854bfe32a4f90647ea826d44a6
Cameron West <camonater62@gmail.com>
2023-03-28 23:18:35 -0700

    Write png file

--- main.cpp
+++ main.cpp
@@ -12,19 +12,23 @@
 #include "sphere.h"
 #include "threadpool.h"
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "external/stb_image_write.h"
+
+#include <array>
 #include <iostream>
 
-color ray_color(const ray& r, const color& background, const hittable& world, int depth) {
+color ray_color(const ray &r, const color &background, const hittable &world, int depth) {
     hit_record rec;
 
     // If we've exceeded the ray bounce limit, no more light is gathered
-    if (depth <= 0) 
+    if (depth <= 0)
         return color(0, 0, 0);
 
     // If the ray hits nothing, return the background color.
-    if (!world.hit(r, 0.001, infinity, rec)) 
+    if (!world.hit(r, 0.001, infinity, rec))
         return background;
-    
+
     ray scattered;
     color attenuation;
     color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
@@ -32,8 +36,8 @@ color ray_color(const ray& r, const color& background, const hittable& world, in
     if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered))
         return emitted;
 
-    return emitted + attenuation * ray_color(scattered, background, world, depth-1);
- }
+    return emitted + attenuation * ray_color(scattered, background, world, depth - 1);
+}
 
 hittable_list two_spheres() {
     hittable_list objects;
@@ -66,7 +70,7 @@ hittable_list random_scene() {
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
             auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
             if ((center - point3(4, 0.2, 0)).length() > 0.9) {
                 shared_ptr<material> sphere_material;
@@ -75,7 +79,7 @@ hittable_list random_scene() {
                     // diffuse
                     auto albedo = color::random() * color::random();
                     sphere_material = make_shared<lambertian>(albedo);
-                    auto center2 = center + vec3(0, random_double(0,.5), 0);
+                    auto center2 = center + vec3(0, random_double(0, .5), 0);
                     world.add(make_shared<moving_sphere>(
                         center, center2, 0.0, 1.0, 0.2, sphere_material));
                 } else if (choose_mat < 0.95) {
@@ -108,7 +112,7 @@ hittable_list random_scene() {
 hittable_list earth() {
     auto earth_texture = make_shared<image_texture>("earthmap.jpg");
     auto earth_surface = make_shared<lambertian>(earth_texture);
-    auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+    auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
 
     return hittable_list(globe);
 }
@@ -117,12 +121,12 @@ hittable_list simple_light() {
     hittable_list objects;
 
     auto pertext = make_shared<noise_texture>(4);
-    objects.add(make_shared<sphere>(point3(0,-1000,0), 1000, make_shared<lambertian>(pertext)));
-    objects.add(make_shared<sphere>(point3(0,2,0), 2, make_shared<lambertian>(pertext)));
+    objects.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
+    objects.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
 
-    auto difflight = make_shared<diffuse_light>(color(4,4,4));
+    auto difflight = make_shared<diffuse_light>(color(4, 4, 4));
     objects.add(make_shared<xy_rect>(3, 5, 1, 3, -2, difflight));
-    objects.add(make_shared<sphere>(point3(0,7,0), 2, difflight));
+    objects.add(make_shared<sphere>(point3(0, 7, 0), 2, difflight));
 
     return objects;
 }
@@ -130,7 +134,7 @@ hittable_list simple_light() {
 hittable_list cornell_box() {
     hittable_list objects;
 
-    auto red   = make_shared<lambertian>(color(0.65, 0.05, 0.05));
+    auto red = make_shared<lambertian>(color(0.65, 0.05, 0.05));
     auto white = make_shared<lambertian>(color(0.73, 0.73, 0.73));
     auto green = make_shared<lambertian>(color(0.12, 0.45, 0.15));
     auto light = make_shared<diffuse_light>(color(15, 15, 15));
@@ -144,22 +148,22 @@ hittable_list cornell_box() {
 
     shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
     box1 = make_shared<rotate_y>(box1, 15);
-    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    box1 = make_shared<translate>(box1, vec3(265, 0, 295));
     objects.add(box1);
 
     shared_ptr<hittable> box2 = make_shared<box>(point3(0, 0, 0), point3(165, 165, 165), white);
     box2 = make_shared<rotate_y>(box2, -18);
     box2 = make_shared<translate>(box2, vec3(130, 0, 65));
-    
+
     objects.add(box2);
-    
+
     return objects;
 }
 
 hittable_list cornell_smoke() {
     hittable_list objects;
 
-    auto red   = make_shared<lambertian>(color(0.65, 0.05, 0.05));
+    auto red = make_shared<lambertian>(color(0.65, 0.05, 0.05));
     auto white = make_shared<lambertian>(color(0.73, 0.73, 0.73));
     auto green = make_shared<lambertian>(color(0.12, 0.45, 0.15));
     auto light = make_shared<diffuse_light>(color(7, 7, 7));
@@ -173,15 +177,15 @@ hittable_list cornell_smoke() {
 
     shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
     box1 = make_shared<rotate_y>(box1, 15);
-    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    box1 = make_shared<translate>(box1, vec3(265, 0, 295));
 
     shared_ptr<hittable> box2 = make_shared<box>(point3(0, 0, 0), point3(165, 165, 165), white);
     box2 = make_shared<rotate_y>(box2, -18);
     box2 = make_shared<translate>(box2, vec3(130, 0, 65));
-    
+
     objects.add(make_shared<constant_medium>(box1, 0.01, color(0, 0, 0)));
     objects.add(make_shared<constant_medium>(box2, 0.01, color(1, 1, 1)));
-    
+
     return objects;
 }
 
@@ -193,14 +197,14 @@ hittable_list final_scene() {
     for (int i = 0; i < boxes_per_side; i++) {
         for (int j = 0; j < boxes_per_side; j++) {
             auto w = 100.0;
-            auto x0 = -1000.0 + i*w;
-            auto z0 = -1000.0 + j*w;
+            auto x0 = -1000.0 + i * w;
+            auto z0 = -1000.0 + j * w;
             auto y0 = 0.0;
             auto x1 = x0 + w;
-            auto y1 = random_double(1,101);
+            auto y1 = random_double(1, 101);
             auto z1 = z0 + w;
 
-            boxes1.add(make_shared<box>(point3(x0,y0,z0), point3(x1,y1,z1), ground));
+            boxes1.add(make_shared<box>(point3(x0, y0, z0), point3(x1, y1, z1), ground));
         }
     }
 
@@ -212,39 +216,34 @@ hittable_list final_scene() {
     objects.add(make_shared<xz_rect>(123, 423, 147, 412, 554, light));
 
     auto center1 = point3(400, 400, 200);
-    auto center2 = center1 + vec3(30,0,0);
+    auto center2 = center1 + vec3(30, 0, 0);
     auto moving_sphere_material = make_shared<lambertian>(color(0.7, 0.3, 0.1));
     objects.add(make_shared<moving_sphere>(center1, center2, 0, 1, 50, moving_sphere_material));
 
     objects.add(make_shared<sphere>(point3(260, 150, 45), 50, make_shared<dielectric>(1.5)));
     objects.add(make_shared<sphere>(
-        point3(0, 150, 145), 50, make_shared<metal>(color(0.8, 0.8, 0.9), 1.0)
-    ));
+        point3(0, 150, 145), 50, make_shared<metal>(color(0.8, 0.8, 0.9), 1.0)));
 
-    auto boundary = make_shared<sphere>(point3(360,150,145), 70, make_shared<dielectric>(1.5));
+    auto boundary = make_shared<sphere>(point3(360, 150, 145), 70, make_shared<dielectric>(1.5));
     objects.add(boundary);
     objects.add(make_shared<constant_medium>(boundary, 0.2, color(0.2, 0.4, 0.9)));
     boundary = make_shared<sphere>(point3(0, 0, 0), 5000, make_shared<dielectric>(1.5));
-    objects.add(make_shared<constant_medium>(boundary, .0001, color(1,1,1)));
+    objects.add(make_shared<constant_medium>(boundary, .0001, color(1, 1, 1)));
 
     auto emat = make_shared<lambertian>(make_shared<image_texture>("earthmap.jpg"));
-    objects.add(make_shared<sphere>(point3(400,200,400), 100, emat));
+    objects.add(make_shared<sphere>(point3(400, 200, 400), 100, emat));
     auto pertext = make_shared<noise_texture>(0.1);
-    objects.add(make_shared<sphere>(point3(220,280,300), 80, make_shared<lambertian>(pertext)));
+    objects.add(make_shared<sphere>(point3(220, 280, 300), 80, make_shared<lambertian>(pertext)));
 
     hittable_list boxes2;
     auto white = make_shared<lambertian>(color(.73, .73, .73));
     int ns = 1000;
     for (int j = 0; j < ns; j++) {
-        boxes2.add(make_shared<sphere>(point3::random(0,165), 10, white));
+        boxes2.add(make_shared<sphere>(point3::random(0, 165), 10, white));
     }
 
     objects.add(make_shared<translate>(
-        make_shared<rotate_y>(
-            make_shared<bvh_node>(boxes2, 0.0, 1.0), 15),
-            vec3(-100,270,395)
-        )
-    );
+        make_shared<rotate_y>(make_shared<bvh_node>(boxes2, 0.0, 1.0), 15), vec3(-100, 270, 395)));
 
     return objects;
 }
@@ -264,124 +263,122 @@ int main() {
     point3 lookat;
     auto vfov = 40.0;
     auto aperture = 0.0;
-    color background(0,0,0);
+    color background(0, 0, 0);
 
     switch (0) {
-        case 1:
-            world = random_scene();
-            background = color(0.7, 0.8, 1);
-            lookfrom = point3(13,2,3);
-            lookat = point3(0,0,0);
-            vfov = 20.0;
-            aperture = 0.1;
-            break;
-        
-        case 2:
-            world = two_spheres();
-            background = color(0.7, 0.8, 1);
-            lookfrom = point3(13,2,3);
-            lookat = point3(0,0,0);
-            vfov = 20.0;
-            break;
+    case 1:
+        world = random_scene();
+        background = color(0.7, 0.8, 1);
+        lookfrom = point3(13, 2, 3);
+        lookat = point3(0, 0, 0);
+        vfov = 20.0;
+        aperture = 0.1;
+        break;
 
-        case 3:
-            world = two_perlin_spheres();
-            background = color(0.7, 0.8, 1);
-            lookfrom = point3(13,2,3);
-            lookat = point3(0,0,0);
-            vfov = 20.0;
-            break;
+    default:
+    case 2:
+        world = two_spheres();
+        background = color(0.7, 0.8, 1);
+        lookfrom = point3(13, 2, 3);
+        lookat = point3(0, 0, 0);
+        vfov = 20.0;
+        break;
 
-        default:
-        case 4:
-            world = earth();
-            lookfrom = point3(13,2,3);
-            lookat = point3(0,0,0);
-            vfov = 20.0;
-            break;
+    case 3:
+        world = two_perlin_spheres();
+        background = color(0.7, 0.8, 1);
+        lookfrom = point3(13, 2, 3);
+        lookat = point3(0, 0, 0);
+        vfov = 20.0;
+        break;
 
-        case 5:
-            world = simple_light();
-            samples_per_pixel = 400;
-            background = color(0, 0, 0);
-            lookfrom = point3(26,3,6);
-            lookat = point3(0,2,0);
-            vfov = 20.0;
-            break;
+    case 4:
+        world = earth();
+        lookfrom = point3(13, 2, 3);
+        lookat = point3(0, 0, 0);
+        vfov = 20.0;
+        break;
 
-        case 6:
-            world = cornell_box();
-            aspect_ratio = 1.0;
-            image_width = 600;
-            samples_per_pixel = 1500;
-            background = color(0,0,0);
-            lookfrom = point3(278, 278, -800);
-            lookat = point3(278, 278, 0);
-            vfov = 40.0;
-            break;
+    case 5:
+        world = simple_light();
+        samples_per_pixel = 400;
+        background = color(0, 0, 0);
+        lookfrom = point3(26, 3, 6);
+        lookat = point3(0, 2, 0);
+        vfov = 20.0;
+        break;
 
-        case 7:
-            world = cornell_smoke();
-            aspect_ratio = 1.0;
-            image_width = 600;
-            samples_per_pixel = 200;
-            lookfrom = point3(278, 278, -800);
-            lookat = point3(278, 278, 0);
-            vfov = 40.0;
-            break;
+    case 6:
+        world = cornell_box();
+        aspect_ratio = 1.0;
+        image_width = 600;
+        samples_per_pixel = 1500;
+        background = color(0, 0, 0);
+        lookfrom = point3(278, 278, -800);
+        lookat = point3(278, 278, 0);
+        vfov = 40.0;
+        break;
 
-        case 8:
-            world = final_scene();
-            aspect_ratio = 1.0;
-            image_width = 800;
-            samples_per_pixel = 10000;
-            background = color(0,0,0);
-            lookfrom = point3(478, 278, -600);
-            lookat = point3(278, 278, 0);
-            vfov = 40.0;
-            break;
+    case 7:
+        world = cornell_smoke();
+        aspect_ratio = 1.0;
+        image_width = 600;
+        samples_per_pixel = 200;
+        lookfrom = point3(278, 278, -800);
+        lookat = point3(278, 278, 0);
+        vfov = 40.0;
+        break;
+
+    case 8:
+        world = final_scene();
+        aspect_ratio = 1.0;
+        image_width = 800;
+        samples_per_pixel = 10000;
+        background = color(0, 0, 0);
+        lookfrom = point3(478, 278, -600);
+        lookat = point3(278, 278, 0);
+        vfov = 40.0;
+        break;
     }
 
-
     // Camera
-    vec3 vup(0,1,0);
+    vec3 vup(0, 1, 0);
     auto dist_to_focus = 10.0;
     int image_height = static_cast<int>(image_width / aspect_ratio);
 
     camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
 
+    shared_ptr<uint8_t[]> pixel_data(new uint8_t[3 * image_width * image_height]);
+
     // Multi-threading
     ThreadPool pool;
-    pool.Start();
 
     // Render
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-
-    for (int j = image_height-1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+    for (int j = image_height - 1; j >= 0; --j) {
         for (int i = 0; i < image_width; ++i) {
-            pool.QueueJob(
-                [
-                    samples_per_pixel, i, j,
-                    image_width, image_height, 
-                    &cam, background, &world
-                ]() {
+            pool.QueueJob([i, j, &image_width, &image_height, &samples_per_pixel, &cam, &background,
+                              &world, &pixel_data]() {
                 color pixel_color(0, 0, 0);
                 for (int s = 0; s < samples_per_pixel; ++s) {
-                    auto u = double(i + random_double()) / (image_width-1);
-                    auto v = double(j + random_double()) / (image_height-1);
+                    auto u = double(i + random_double()) / (image_width - 1);
+                    auto v = double(j + random_double()) / (image_height - 1);
                     ray r = cam.get_ray(u, v);
                     pixel_color += ray_color(r, background, world, max_depth);
                 }
-                write_color(std::cout, pixel_color, samples_per_pixel);
+                write_color(
+                    pixel_data, i, j, image_width, image_height, pixel_color, samples_per_pixel);
             });
         }
     }
 
-    while (pool.Busy()) {
-        ;
-    }
+    pool.Start();
+    pool.Wait();
     pool.Stop();
 
-    std::cerr << "\nDone.\n";
+    int res = stbi_write_png(
+        "image.png", image_width, image_height, 3, pixel_data.get(), 3 * image_width);
+
+    std::cout << "\nDone.\n";
+
+    return 0;
 }
--- threadpool.h
+++ threadpool.h
@@ -1,27 +1,28 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
-#include <functional>
-#include <vector>
-#include <thread>
-#include <queue>
 #include <condition_variable>
+#include <functional>
+#include <queue>
+#include <thread>
+#include <vector>
 
 class ThreadPool {
-    public:
-        void Start();
-        void QueueJob(const std::function<void()>& job);
-        void Stop();
-        bool Busy();
+public:
+    void Start();
+    void QueueJob(const std::function<void()> &job);
+    void Stop();
+    bool Busy();
+    void Wait();
 
-    private:
-        void ThreadLoop();
+private:
+    void ThreadLoop();
 
-        bool should_terminate = false;
-        std::mutex queue_mutex;
-        std::condition_variable mutex_condition;
-        std::vector<std::thread> threads;
-        std::queue<std::function<void()>> jobs;
+    bool should_terminate = false;
+    std::mutex queue_mutex;
+    std::condition_variable mutex_condition;
+    std::vector<std::thread> threads;
+    std::queue<std::function<void()>> jobs;
 };
 
 void ThreadPool::Start() {
@@ -37,8 +38,8 @@ void ThreadPool::ThreadLoop() {
         std::function<void()> job;
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
-            mutex_condition.wait(lock, [this] {
-                return !jobs.empty() || should_terminate;
+            mutex_condition.wait(lock, [this] { 
+                return !jobs.empty() || should_terminate; 
             });
             if (should_terminate) {
                 return;
@@ -50,7 +51,7 @@ void ThreadPool::ThreadLoop() {
     }
 }
 
-void ThreadPool::QueueJob(const std::function<void()>& job) {
+void ThreadPool::QueueJob(const std::function<void()> &job) {
     {
         std::unique_lock<std::mutex> lock(queue_mutex);
         jobs.push(job);
@@ -73,10 +74,16 @@ void ThreadPool::Stop() {
         should_terminate = true;
     }
     mutex_condition.notify_all();
-    for (std::thread& active_thread : threads) {
+    for (std::thread &active_thread : threads) {
         active_thread.join();
     }
     threads.clear();
+}
+
+void ThreadPool::Wait() {
+    while (Busy()) {
+        std::this_thread::yield();
+    }
 }
 
 #endif
